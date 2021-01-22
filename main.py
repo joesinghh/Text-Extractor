6072c406d597f6f77c0bbf829df86a9359f61614
Joe-Sin7h <josingh481@gmail.com>
2021-01-22 20:53:31 +0530

    fixed bugs and added test image

--- main.py
+++ main.py
@@ -6,20 +6,21 @@ from PIL import Image, ImageTk
 from threading import Thread
 from tesseract import ProcessImage
 
-IMAGE = {}
-
-def extractimg(img):
-    
+#display result
+def extractimg(img): 
     result = Toplevel(mainframe)
+    result.title('Result')
     result.geometry('300x300')
-    Label(result,text=ProcessImage(img),wrap=True).place(relwidth=1,relheight=1)
+    text = Text(result,wrap=WORD)
+    text.place(relwidth=1,relheight=1)
+    text.insert(END,ProcessImage(img))
 
-
+#camera Cature display
 def display():
 
     rep,frame = cap.read()
     frame = cv2.flip(frame,1)
-    cv2img = cv2.cvtColor(frame,cv2.COLOR_BGR2RGBA)
+    cv2img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     img = Image.fromarray(cv2img)
     imgtk = ImageTk.PhotoImage(image=img)
     screen.imgtk = imgtk
@@ -27,22 +28,25 @@ def display():
     if val==0:
         camera.destroy()
         cap.release()
-        extractimg(cv2img)
-        
-        
+        img = img.convert('RGB')
+        img = img.save("test.jpg","JPEG",quality=80, optimize=True, progressive=True)
+        extractimg('test.jpg')
         
     else:
         camera.after(10,display)
 
+#select image and extract text
 def open_img():
     path = askopenfile(title="Select Image",filetypes=[("Jpg",".jpg"),("Png",".png")])
     if path!=None:
-        img = Image.open(path)
-        save_img(img)
+        extractimg(path.name)
 
+#stop camera display
 def snap_():
     global val
     val=0
+
+#open camera window
 def opencam():
     global val ,screen,cap,camera
     camera = Toplevel(mainframe)
@@ -53,12 +57,15 @@ def opencam():
     snap.place(relheight=0.1,relwidth=0.5,relx=0.4,rely=0.9)
     screen = Label(camera)
     screen.place(relheight=0.90,relwidth=1)
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0) #Video capture object ( 0 - default camera , 1 - second camera , and so on)
     display()
 
+#main window
 root = Tk()
-root.geometry('600x600');
-root.title("Application");
+#size of window
+root.geometry('600x600'); 
+root.title("Text Extractor");
+
 #Frames
 startframe = Frame(root)
 mainframe = Frame(root)
@@ -69,15 +76,14 @@ saveas.place(relheight=1,relwidth=1)
 
 startframe.tkraise()
 
+#Mainframe
 opencam = Button(mainframe,text='Open Cam',command=opencam)
 opencam.place(relheight=0.1,relwidth=0.3,relx=0.2,rely=0.2)
 
 selectimg = Button(mainframe,text='Select Image',command = open_img);
 selectimg.place(relheight=0.1,relwidth=0.3,relx=0.5,rely=0.2)
 
-extract = Button(mainframe,text='Extract',command=extractimg)
-extract.place(relwidth=0.4,relheight=0.1,relx=0.25,rely=0.5)
-
+#start Frame
 Start = Button(startframe,text='start',command= mainframe.tkraise)
 Start.place(relheight=0.15,relwidth=0.3,relx=0.4,rely=0.4)
 
--- tesseract.py
+++ tesseract.py
@@ -1,5 +1,5 @@
 import cv2
-
+import numpy as np
 import pytesseract as pt
 pt.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
@@ -16,15 +16,25 @@ def remove_noise(image):
 def thresholding(image):
     return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
+def opening(image):
+    kernel = np.ones((5,5),np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
 
 def ProcessImage(img):
     image = cv2.imread(img)
     rn = remove_noise(image)
     gray = get_grayscale(rn)
-    thresholding = thresholding(gray)
+    openin = opening(gray)
+    thresh  = thresholding(openin)
+    # cv2.imwrite('testout2.jpg',thresh)
+    text = pt.image_to_string(thresh,lang='eng')
+    text = text.split(' ')
+    textoutput = ''
+    for i in text:
+        textoutput+=' '+i
 
-    return pt.image_to_string(thresholding,lang='eng')
+    return textoutput
 
 
 if __name__=='__main__':
-    pass
+    print(ProcessImage('test.jpg'))
